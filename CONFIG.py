f01cb0d46fe9b68573d7c645858b1e60b6f7d6b6
Daniel Moreira <moreiradefranca.daniel@gmail.com>
2026-05-25 00:51:29 -0300

    CONFIG + Demeter boons list

---
+++ CONFIG.py
@@ -0,0 +1,29 @@
+DEUSES = {
+    'Zeus': [
+
+    ],
+    'Ares': [
+
+    ],
+    'Atenas': [
+
+    ],
+    'Hermes': [
+
+    ],
+    'Afrodite': [
+
+    ],
+    'Poseidon': [
+
+    ],
+    'Artemis': [
+
+    ],
+    'Dionisio': [
+
+    ],
+    'Demeter': [
+        'Frost Strike', 'Frost Flourish', 'Crystal Beam', 'Icy Flare', 'Mistral Dash', "Demeter's Aid", 'Frozen Touch', 'Rare Crop', 'Ravenous Will', 'Nourished Soul', 'Snow Burst', 'Arctic Blast', 'Killing Freeze', 'Glacial Glare', 'Winter Harvest',
+    ],
+}
